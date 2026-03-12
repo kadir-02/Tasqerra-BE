97ed6cc04a5411b129b6a9bf2e6305f99010eff5
Kadir <kadir.janaka@consociatesolutions.com>
2026-03-12 15:22:32 +0530

    google auth update

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import http from "http";
 import app from "./app";
-// import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
-
-
 
 const server = http.createServer(app);
 
